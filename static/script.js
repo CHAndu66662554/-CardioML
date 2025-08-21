160e9c2871bb84d3ea5b998042a5b3ae91b2abf6
CHAndu66662554 <169144168+CHAndu66662554@users.noreply.github.com>
2025-08-22 01:15:15 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -15,7 +15,7 @@ document.getElementById('predictionForm').addEventListener('submit', async (e) =
     
     try {
         // Send data to Flask backend
-        const response = await fetch('http://localhost:5000/predict', {
+        const response = await fetch('https://cardioml-1.onrender.com//predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
